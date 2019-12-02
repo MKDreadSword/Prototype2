ee8da37fb1d114c69025a8fa6d4e5fe0e5f767ff
MKDreadSword <blueninja0115@gmail.com>
2019-12-02 10:58:54 -0800

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,8 +12,13 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
     void Update()
-    {
+    { 
+        if (transform.position.x > -xRange)
+        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
