bd4d07415eabe98d632c5a5603b38cef1ec420f1
MKDreadSword <blueninja0115@gmail.com>
2019-12-02 12:00:04 -0800

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,13 +14,14 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10;
+
     void Update()
-    { 
-        if (transform.position.x > -xRange)
-        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    {
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        if (transform.position.x > xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
